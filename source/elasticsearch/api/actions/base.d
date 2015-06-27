11347857d84bbfc346f4c02beee79e20cf4b8a4c
David <dkdewitt@gmail.com>
2015-06-27 15:38:58 -0700

    Fixed bad commit :)

--- source/elasticsearch/api/actions/base.d
+++ source/elasticsearch/api/actions/base.d
@@ -5,59 +5,99 @@ import elasticsearch.api.base;
 alias create = elasticsearch.api.actions.base.index;
 
 Response index(Client client, Parameters arguments = Parameters()) {
-	RequestMethod method;
+    RequestMethod method;
 
-	arguments.enforceParameter("index");
-	arguments.enforceParameter("type");
+    arguments.enforceParameter("index");
+    arguments.enforceParameter("type");
 
-	auto requestBody = arguments["body"];
-	auto params = arguments.validateAndExtract(
-		"consistency", "op_type", "parent", "percolate", "refresh", "replication", "routing",
-		"timeout", "timestamp", "ttl", "version", "version_type"
-	);
-	string[] path = [arguments["index"], arguments["type"]];
+    auto requestBody = arguments["body"];
+    auto params = arguments.validateAndExtract(
+        "consistency", "op_type", "parent", "percolate", "refresh", "replication", "routing",
+        "timeout", "timestamp", "ttl", "version", "version_type"
+    );
+    string[] path = [arguments["index"], arguments["type"]];
 
-	if (arguments.hasField("id")) {
-		method = RequestMethod.PUT;
-		path ~= arguments["id"];
-	}
-	else {
-		method = RequestMethod.POST;
-	}
+    if (arguments.hasField("id")) {
+        method = RequestMethod.PUT;
+        path ~= arguments["id"];
+    }
+    else {
+        method = RequestMethod.POST;
+    }
 
-	return client.performRequest(method, esPathify(path), params, requestBody);
+    return client.performRequest(method, esPathify(path), params, requestBody);
 }
 
 Response index(Client client, string indexName, string type, string id, string requestBody, Parameters p = Parameters()) {
-	p["index"] = indexName;
-	p["type"] = type;
-	p["id"] = id;
-	p["body"] = requestBody;
+    p["index"] = indexName;
+    p["type"] = type;
+    p["id"] = id;
+    p["body"] = requestBody;
 
-	return index(client, p);
+    return index(client, p);
 }
 
 Response index(Client client, string indexName, string type, string requestBody, Parameters p = Parameters()) {
-	p["index"] = indexName;
-	p["type"] = type;
-	p["body"] = requestBody;
-	
-	return index(client, p);
+    p["index"] = indexName;
+    p["type"] = type;
+    p["body"] = requestBody;
+    
+    return index(client, p);
+}
+
+Response bulkIndex(Client client, Parameters arguments = Parameters()) {
+    RequestMethod method;
+
+    arguments.enforceParameter("index");
+    arguments.enforceParameter("type");
+
+    auto requestBody = arguments["body"];
+    auto params = arguments.validateAndExtract(
+        "consistency", "op_type", "parent", "percolate", "refresh", "replication", "routing",
+        "timeout", "timestamp", "ttl", "version", "version_type","bulk"
+    );
+    string[] path = [arguments["index"], arguments["type"],"_bulk"];
+
+    if (arguments.hasField("id")) {
+        method = RequestMethod.PUT;
+        path ~= arguments["id"];
+    }
+    else {
+        method = RequestMethod.POST;
+    }
+
+    return client.performRequest(method, esPathify(path), params, requestBody);
+}
+
+Response bulkIndex(Client client, string indexName, string type, string requestBody, Parameters p = Parameters()) {
+    p["index"] = indexName;
+    p["type"] = type;
+    p["body"] = requestBody;
+    p["bulk"] = "true";
+    return bulkIndex(client, p);
 }
 
 Response search(Client client, Parameters arguments = Parameters()) {
-	string index = arguments.hasField("index") ? arguments["index"] : "_all";
-	string type = arguments.hasField("type") ? arguments["type"] : "";
+    string index = arguments.hasField("index") ? arguments["index"] : "_all";
+    string type = arguments.hasField("type") ? arguments["type"] : "";
 
-	auto params = arguments.validateAndExtract(
-		"analyzer", "analyze_wildcard", "default_operator", "df", "explain", "fields", "from",
-		"ignore_indices", "ignore_unavailable", "allow_no_indices", "expand_wildcards", "lenient",
-		"lowercase_expanded_terms", "preference", "q", "routing", "scroll", "search_type", "size", "sort",
-		"source", "_source", "_source_include", "source_exclude", "stats", "suggest_field", "suggest_mode",
-		"suggest_size", "suggest_text", "timeout", "version"
-	);
+    auto params = arguments.validateAndExtract(
+        "analyzer", "analyze_wildcard", "default_operator", "df", "explain", "fields", "from",
+        "ignore_indices", "ignore_unavailable", "allow_no_indices", "expand_wildcards", "lenient",
+        "lowercase_expanded_terms", "preference", "q", "routing", "scroll", "search_type", "size", "sort",
+        "source", "_source", "_source_include", "source_exclude", "stats", "suggest_field", "suggest_mode",
+        "suggest_size", "suggest_text", "timeout", "version"
+    );
 
-	auto path = esPathify(esListify(index), esListify(type), "_search");
+    auto path = esPathify(esListify(index), esListify(type), "_search");
 
-	return client.performRequest(RequestMethod.GET, path, params, arguments["body"]);
+    return client.performRequest(RequestMethod.GET, path, params, arguments["body"]);
+}
+
+Response scroll(Client client, Parameters arguments = Parameters()) {
+    auto params = arguments.validateAndExtract("scroll", "scroll_id");
+    
+    auto path = esPathify("_search", "scroll");
+
+    return client.performRequest(RequestMethod.GET, path, params);
 }
--- source/elasticsearch/transport/http/vibe.d
+++ source/elasticsearch/transport/http/vibe.d
@@ -13,63 +13,68 @@ alias VibeLogLevel = vibe.core.log.LogLevel;
 alias ESLogLevel = elasticsearch.transport.transport.LogLevel;
 
 HTTPMethod vibeTransportRequestMethod(RequestMethod method) {
-	final switch(method) {
-		case RequestMethod.HEAD: return HTTPMethod.HEAD;
-		case RequestMethod.GET: return HTTPMethod.GET;
-		case RequestMethod.POST: return HTTPMethod.POST;
-		case RequestMethod.PUT: return HTTPMethod.PUT;
-		case RequestMethod.DELETE: return HTTPMethod.DELETE;
-	}
+    final switch(method) {
+        case RequestMethod.HEAD: return HTTPMethod.HEAD;
+        case RequestMethod.GET: return HTTPMethod.GET;
+        case RequestMethod.POST: return HTTPMethod.POST;
+        case RequestMethod.PUT: return HTTPMethod.PUT;
+        case RequestMethod.DELETE: return HTTPMethod.DELETE;
+    }
 }
 
 class VibeTransport : Transport {
 override:
-	protected void transportLog(ESLogLevel level, string message) {
-		final switch(level) {
-			case ESLogLevel.debug_: logDebug(message);
-				break;
-			case ESLogLevel.info: logInfo(message);
-				break;
-			case ESLogLevel.error: logError(message);
-				break;
-		}
-		
-	}
-	
-	@property string protocol() { return "http"; }
-	
-	Response performTransportRequest(Connection connection, RequestMethod method, string path, Parameters parameters, string requestBody = "") {		Response response;
-		requestHTTP(connection.fullURL(path, parameters),
-		            (scope req) {
-			req.method = vibeTransportRequestMethod(method);
-			if (requestBody != "") {
-				auto json = parseJsonString(requestBody);
-				req.writeJsonBody(json);
-			}
-		},
-		(scope res) {
-			response.status = res.statusCode;
-			response.headers = res.headers;
-			if (method != RequestMethod.HEAD) {
-				if (res.statusCode >= 200 && res.statusCode < 300) {
-					auto responseBody = res.bodyReader.readAllUTF8();
-					response.responseBody = responseBody;
-				}
-				else {
-					switch(res.statusCode) {
-						case(HTTPStatus.gatewayTimeout, HTTPStatus.requestTimeout): 
-							throw new HostUnreachableException(connection);
-						default: 
-							auto responseBody = res.bodyReader.readAllUTF8();
-							response.responseBody = responseBody;
-							throw new RequestException(connection, method, path, parameters, requestBody, response);
-					}
-				}
-			}
-		}
-		);
-		
-		return response;
-	}
-	
+    protected void transportLog(ESLogLevel level, string message) {
+        final switch(level) {
+            case ESLogLevel.debug_: logDebug(message);
+                break;
+            case ESLogLevel.info: logInfo(message);
+                break;
+            case ESLogLevel.error: logError(message);
+                break;
+        }
+        
+    }
+    
+    @property string protocol() { return "http"; }
+    
+    Response performTransportRequest(Connection connection, RequestMethod method, string path, Parameters parameters, string requestBody = "") {        Response response;
+        requestHTTP(connection.fullURL(path, parameters),
+                    (scope req) {
+            req.method = vibeTransportRequestMethod(method);
+            if (requestBody != "") {
+                if (parameters.get("bulk",null)){
+                    req.writeBody(cast(ubyte[])requestBody, "application/json");
+                }
+                else{
+                auto json = parseJsonString(requestBody);
+                req.writeJsonBody(json);
+            }
+            }
+        },
+        (scope res) {
+            response.status = res.statusCode;
+            response.headers = res.headers;
+            if (method != RequestMethod.HEAD) {
+                if (res.statusCode >= 200 && res.statusCode < 300) {
+                    auto responseBody = res.bodyReader.readAllUTF8();
+                    response.responseBody = responseBody;
+                }
+                else {
+                    switch(res.statusCode) {
+                        case(HTTPStatus.gatewayTimeout, HTTPStatus.requestTimeout): 
+                            throw new HostUnreachableException(connection);
+                        default: 
+                            auto responseBody = res.bodyReader.readAllUTF8();
+                            response.responseBody = responseBody;
+                            throw new RequestException(connection, method, path, parameters, requestBody, response);
+                    }
+                }
+            }
+        }
+        );
+        
+        return response;
+    }
+    
 }
